2bb53bb9fe24ad227502ef7614f14545283dbe45
Matthew <rhr6090@autuni.ac.nz>
2024-02-25 19:39:09 +1300

    Working New Identity assessment container

---
+++ identity_assessment/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+echo "Welcome to ciso360ai"
+
+pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
+
+
