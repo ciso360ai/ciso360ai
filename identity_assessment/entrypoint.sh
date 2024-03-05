4828bef1d43c4c592e7f1b2a0f348802d828441a
Matthew <rhr6090@autuni.ac.nz>
2024-03-05 13:25:48 +1300

    Created the Skeleton of the POC
    Created the skeleton of the POC. Adding in the initial elements for API and database fetching.
    
    I also left a couple of pseudo code block so Sahim can see where things are planned to go

--- identity_assessment/entrypoint.sh
+++ identity_assessment/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Welcome to ciso360ai"
 
-python3 "/usr/local/bin/startup"
+python3 "/usr/local/bin/poc"
 
 pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
