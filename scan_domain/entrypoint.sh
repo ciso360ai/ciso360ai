5c69507fa7f86a723695e6f51f7c480fd0d1cf79
faustro <faustin@altersec.com>
2024-03-29 12:31:25 +1300

    updates

--- scan_domain/entrypoint.sh
+++ scan_domain/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
+echo "Starting Domain Scanner ..."
+
 pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
--- scan_email/entrypoint.sh
+++ scan_email/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-echo "Starting Email Scanner"
+echo "Starting Email Scanner ..."
 
 pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
