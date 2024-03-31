09cf02c2150b76d52098c59ae114eeec45df80a0
faustro <faustin@altersec.com>
2024-04-01 09:35:32 +1300

    sleep entrypoint

--- scan_domain/entrypoint.sh
+++ scan_domain/entrypoint.sh
@@ -2,4 +2,6 @@
 
 echo "Starting Domain Scanner ..."
 
+sleep 20
+
 pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
