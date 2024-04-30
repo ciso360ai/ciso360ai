33ce3fe4d84b2fdedb73b7e57046ebf53089769b
Matthew Buckley <128117141+DemonicFriend@users.noreply.github.com>
2024-04-30 15:29:52 +1200

    Update repository from Ciso360ai/Ciso360ai (#3)
    * updates
    
    * fix demo user
    
    * sleep entrypoint
    
    * add ips as inactive
    
    * chore(deps): update docker/setup-buildx-action action to v3.3.0
    
    * pg version fix
    
    * Updates
    
    * delete obsolete verison in docker
    
    * fix ipv6 issue
    
    * fix ip status issue
    
    * update project
    
    * python break-system-packages
    
    * add tags
    
    ---------
    
    Co-authored-by: faustro <faustin@altersec.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Faustin Roman <28839310+faustro@users.noreply.github.com>

--- api/migrations/30000101B-Add-Demo-Users.cjs
+++ api/migrations/30000101B-Add-Demo-Users.cjs
@@ -3,7 +3,7 @@ module.exports = {
         await knex.schema.raw(`
 		INSERT INTO directus_roles (id, name, icon, description, ip_access, enforce_tfa, admin_access, app_access) VALUES ('fb9a9ff6-f729-481f-9c27-f152f30d4daa', 'DEMOuser', 'supervised_user_circle', NULL, NULL, false, false, true);
 
-		INSERT INTO directus_users (id, first_name, last_name, password, status, role) VALUES ('ca4d7676-f94d-4561-a84f-4db18fba6d21', 'Demo', 'User', 'demo@ciso360.ai', 'draft', 'fb9a9ff6-f729-481f-9c27-f152f30d4daa');
+		INSERT INTO directus_users (id, first_name, last_name, email, status, role) VALUES ('ca4d7676-f94d-4561-a84f-4db18fba6d21', 'Demo', 'User', 'demo@ciso360.ai', 'draft', 'fb9a9ff6-f729-481f-9c27-f152f30d4daa');
 
 		INSERT INTO directus_roles (id, name, icon, description, ip_access, enforce_tfa, admin_access, app_access) VALUES ('0202a11b-1614-46b9-b73f-e53a7a765186', 'SYS Images', 'supervised_user_circle', NULL, NULL, false, false, false);
 
--- scan_email/entrypoint.sh
+++ scan_email/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-echo "Starting Email Scanner"
+echo "Starting Email Scanner ..."
+
+sleep 20
 
 pg_timetable postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB -c=$HOSTNAME --log-file=/logs/$HOSTNAME.log "$@"
